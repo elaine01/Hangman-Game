9d47cdfa1efb82f149541176de4db89074fcb870
elaine01 <ecumming.g@gmail.com>
2017-11-18 16:52:40 -0800

    update

---
+++ assets/javascript/game3.js
@@ -0,0 +1,104 @@
+// Default counter
+var wins = 0; // (# of times the user has guessed correctly)
+var guessesLeft = 9; // (# of guesses left. This will update)
+
+// create an array of words
+var wordsArr = ["cat", "dog", "penguin", "bats"]; // answer choices
+
+// choose word randomly
+var currentWord = wordsArr[Math.floor(Math.random()*wordsArr.length)]; // randomize words for user to guess
+var underScore = []; // underscore based on length of word
+
+// Full list of choices user can choose from
+// var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var currentGuessed = []; // user's guesses so far in a round
+var rightWord = []; // user's correct guesses
+var wrongWord = []; // user's incorrect guesses
+
+
+// create underscores based on length of word
+function generateUnderscore() {
+	underScore = [];
+	currentWord = wordsArr[Math.floor(Math.random()*wordsArr.length)];
+	console.log(currentWord);
+	for (i = 0; i < currentWord.length; i++) {
+		underScore.push('_');
+		document.querySelector("#docUnderScore").innerHTML = underScore.join(' ');
+		console.log(document.querySelector("#docUnderScore") + " generate");
+		console.log(underScore + " generate1");
+	}
+}
+
+// resets to new round
+var reset = function() {
+	guessesLeft = 9;
+	currentGuessed = [];
+	rightWord = [];
+	wrongWord = [];
+	document.querySelector(".guesses").innerHTML = "Number of guesses remaining: 9";
+	document.querySelector("#wrongGuess").innerHTML = "";
+	generateUnderscore();
+	}
+
+reset();
+
+
+// get user's guess
+document.onkeyup = function(event) {
+	if (event.key === "Meta") {
+		return;	
+	}
+
+	var userGuess = event.key.toLowerCase();
+	console.log(userGuess);
+	var indexNum = currentWord.indexOf(userGuess);
+	// // if user's guess != a letter
+	// if (letters.indexOf(userGuess) === -1) {
+	// 	return alert("You must pick a letter to play!");
+	// } // if user already guessed that letter
+	// if (currentGuessed.indexOf(userGuess) !== -1) {
+	// 	return alert("You already guessed that letter. Pick something else!");
+	// }
+
+	// if user's guess is right
+	if (indexNum > -1) { 
+		// add to right words array
+		for (i = 0; i < currentWord.length; i++) {
+			if (currentWord[i] === userGuess) {
+				// replace underscore with right letter
+				underScore[currentWord.indexOf(userGuess)] = userGuess;
+				docUnderScore.innerHTML = underScore.join(" ");
+				console.log(underScore.join(" "));
+			}
+			// docdocUnderScore.innerHTML = rightWord;
+			if (currentWord.indexOf(userGuess) === -1) {
+				document.getElementById("docUnderScore").innerHTML = underScore.join(' ');
+				wrongWord.push(userGuess);
+				document.getElementById("wrongGuess").innerHTML = wrongWord.join(' ');
+			}
+			if (underScore.join('') === currentWord) {
+			// console.log(underScore + " in if");
+			wins++;
+			document.querySelector(".win").innerHTML = "Wins: " + wins;
+			reset();
+			}
+		}
+	} else { // if user's guess is wrong
+		if (guessesLeft === 0) { // if user has 0 guesses left
+			alert("You lose!");
+			reset();
+		} else {
+			wrongWord.push(userGuess);
+			console.log(document.getElementById("wrongGuess"));
+			console.log(wrongWord);
+			document.getElementById("wrongGuess").innerHTML = wrongWord.join(' ');	
+			guessesLeft--;
+			document.querySelector(".guesses").innerHTML = "Number of guesses remaining: " + guessesLeft;	
+		}  
+	}  
+};
+
+// docUnderScore[0].innerHTML = generateUnderscore().join(" ");
+
+
+
